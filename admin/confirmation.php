dff1ff02d6b19b8740b946df1c8a6d79edead680
Rahul Negi <83061095+RAHUL24NEGI@users.noreply.github.com>
2023-06-08 22:27:56 +0530

    Delete admin directory

--- admin/confirmation.php
+++
@@ -1,3 +0,0 @@
-<div align="center">
-    <font color="green"><h1>FILE UPLOADED SUCCESFULLY</h1></font>
-</div>
--- admin/delete_subinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('../dbconfig.php');
-	
-	$info=$_GET['subcode'];
-	
-	mysqli_query($conn,"delete from subinfo where subcode='$info'");
-	header('location:dashboard.php?info=display_subinfo');
-?>
